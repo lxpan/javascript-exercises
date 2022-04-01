c3115a217b63d94798839efe5b23b7fa777d8565
Luoxi Pan <l.panpax@gmail.com>
2022-04-01 17:03:52 +1100

    Sum interval of numbers - from smaller to larger

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    sum = 0;
 
+    for(let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
