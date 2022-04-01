7145a8911eade815f7600370c373abdb9df693c4
Luoxi Pan <l.panpax@gmail.com>
2022-04-01 17:52:45 +1100

    Implement temperature conversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+  celsius = (fahrenheit - 32) * (5/9)
 
+  if (celsius % 1 != 0) {
+    return celsius.toFixed(1);
+  } else {
+    return celsius;
+  }
+  
 };
 
-const ctof = function() {
-
+// celsius to fahrenheit
+const ctof = function(celsius) {
+  fahrenheit = (celsius * (9/5)) + 32
+  return fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
