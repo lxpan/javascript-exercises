551c9d80b059ecdf5a892312040670655b9fbff8
Luoxi Pan <l.panpax@gmail.com>
2022-04-01 17:11:56 +1100

    Handle non number and negative arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
 const sumAll = function(start, end) {
     sum = 0;
 
+    if(start < 0 || end < 0) {
+        return 'ERROR'
+    }
+
+    else if(typeof(start) !== 'number' || typeof(end) !== 'number') {
+        return 'ERROR'
+    }
+
     for(let i = start; i <= end; i++) {
         sum += i;
     }
