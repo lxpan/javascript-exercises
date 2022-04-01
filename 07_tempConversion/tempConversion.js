7808667dcfb8d3c7e2ae6b7009b68fe98b65d48b
Luoxi Pan <l.panpax@gmail.com>
2022-04-01 20:23:42 +1100

    ctof: convert decimal string to Number

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,6 +1,6 @@
 const ftoc = function(fahrenheit) {
   celsius = (fahrenheit - 32) * (5/9)
-
+  // if not whole number, round to 1 dp
   if (celsius % 1 != 0) {
     return Number(celsius.toFixed(1));
   } else {
@@ -12,7 +12,12 @@ return celsius;
 // celsius to fahrenheit
 const ctof = function(celsius) {
   fahrenheit = (celsius * (9/5)) + 32
-  return fahrenheit.toFixed(1);
+  // if not whole number, round to 1 dp
+  if (fahrenheit % 1 != 0) {
+    return Number(fahrenheit.toFixed(1));
+  } else {
+    return fahrenheit;
+  }
 };
 
 // Do not edit below this line
