4ef9d55c2c02e7c700f4c3a8451d96ad5f3ffb9a
Luoxi Pan <l.panpax@gmail.com>
2022-04-08 16:19:49 +1000

    Add solution using reduce()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -16,5 +16,21 @@ const findTheOldest = function(array) {
     return sorted[0];
 };
 
+const _findTheOldest = function(array) {
+    return array.reduce((oldest, current) => {
+        oldestAge = _getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        currentAge = _getAge(current.yearOfBirth, current.yearOfDeath);
+
+        return (currentAge > oldestAge) ? current : oldest;
+    });
+}
+
+function _getAge(birth, death) {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+
 // Do not edit below this line
 module.exports = findTheOldest;
