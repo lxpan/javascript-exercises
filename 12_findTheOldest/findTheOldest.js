4ee7d6171d634feff393757e338fa3ef4830db30
Luoxi Pan <l.panpax@gmail.com>
2022-04-08 16:04:29 +1000

    Complete findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const sorted = array.sort((a, b) => {
+        const currentYear = new Date().getFullYear();
+        
+        let ageA = a.yearOfDeath - a.yearOfBirth;
+        let ageB = b.yearOfDeath - b.yearOfBirth;
 
+        if(a.hasOwnProperty('yearOfDeath') === false) {
+            ageA = currentYear - a.yearOfBirth;
+        } else if(b.hasOwnProperty('yearOfDeath') === false) {
+            ageB = currentYear - b.yearOfBirth;
+        } 
+
+        return (ageA > ageB) ? -1 : 1;
+    });
+    return sorted[0];
 };
 
 // Do not edit below this line
