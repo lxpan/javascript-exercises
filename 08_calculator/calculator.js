5117de273cf4a157ede9a730aa44c2a61c37b7e4
Luoxi Pan <l.panpax@gmail.com>
2022-04-08 14:57:52 +1000

    Complete calculator excercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,57 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	const _sum = numbers.reduce((total, value) => {
+    return total + value;
+  }, 0);
+  return _sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  const result = numbers.reduce((product, value) => {
+    return product * value;
+  })
+  return result;
 };
 
-const power = function() {
-	
+// const power = function(value, power) {
+// 	return 4**3;
+// };
+
+const power = function(value, power) {
+  product = value;
+	for(let i = 1; i < power; i++) {
+    product = product * value;
+  }
+  return product;
 };
 
-const factorial = function() {
-	
-};
+// const factorial = function(n) {
+//   let product = 1;
+//   for(let i = n; i > 0; i--) {
+//     product *= i;
+//   }
+//   return product;
+// };
+
+function factorial(n) {
+  if(n == 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+// f(3) = 3 * 2 = 6
+// f(3) = 3 * f(2)=[2]
+// f(2) = 2 * f(1)=[1]
+// f(1) = 1 * f(0)=[1]
+// f(0) = 1
 
 // Do not edit below this line
 module.exports = {
