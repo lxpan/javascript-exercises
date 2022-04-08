25671c1e2a7eee06dbad71c063000d3480fc6baa
Luoxi Pan <l.panpax@gmail.com>
2022-04-08 15:19:37 +1000

    Complete palindrome challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function () {
+// const palindromes = function (str) {
+//     // set index to last char
+//     // iterate backwards until front, appending characters
+//     // check if result == initial word    
+//     clean_string = str
+//                 .replace(/\W/g, '')
+//                 .toLowerCase();
+    
+//     reverse = '';
+    
+//     for(let i = clean_string.length - 1; i >= 0; i--) {
+//         reverse += clean_string[i];
+//     }
+//     return reverse == clean_string;
+// };
 
+const palindromes = function (str) {
+    // set index to last char
+    // iterate backwards until front, appending characters
+    // check if result == initial word
+    clean_string = str.replace(/\W/g, '').toLowerCase();
+
+    const isPalindrome = clean_string
+                .split("")
+                .reverse()
+                .join("") == clean_string;
+    
+    return isPalindrome;
 };
 
 // Do not edit below this line
